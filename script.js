fddb0d21eb9ebc4d41448d50e8c57e767a075505
plikipe <plikipie@gmail.com>
2020-09-28 15:18:17 +0700

    fix js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let navSection = document.querySelectorAll("nav li");
 
 // for click event
-navSection.forEach((c) => {
-    c.onclick = () => {
+navSection.forEach((v) => {
+    v.onclick = () => {
         setTimeout(() => {
-            menuSection.forEach((j) => j.classList.remove("active"));
-            c.classList.add("active");
+            navSection.forEach((j) => j.classList.remove("active"));
+            v.classList.add("active");
         }, 300);
     };
 });
 // for window scrolldown event
 
 window.onscroll = () => {
-    let navSection = document.querySelectorAll("nav section");
+    let mainSection = document.querySelectorAll("main section");
 
-    navSection.forEach((c, j) => {
-        let rect = c.getBoundingClientRect().x;
-        if (rect < window.innerHeight - 200) {
+    mainSection.forEach((c, j) => {
+        let rect = c.getBoundingClientRect().y;
+        if (rect < window.innerHeight - 550) {
             navSection.forEach((c) => c.classList.remove("active"));
             navSection[j].classList.add("active");
         }
